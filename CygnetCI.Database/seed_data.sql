57d9840470f6c0e6e7d2a59065875e3ffa5b7c7b
KBANK\KMBL199081 <dhanashri.bhure@kotak.com>
2025-11-24 13:59:35 +0530

    chatgpt code removed sql part

--- CygnetCI.Database/seed_data.sql
+++
@@ -1,3 +0,0 @@
-﻿-- seed_data.sql
-INSERT INTO roles (name, description) VALUES (''admin'',''Administrator role'') ON CONFLICT DO NOTHING;
-INSERT INTO roles (name, description) VALUES (''developer'',''Developer role'') ON CONFLICT DO NOTHING;
