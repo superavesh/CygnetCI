b41eae685e251472e12bbbb5048191e3155955f2
CYGNET\amkhatri <avesh.khatri@cygnet.one>
2026-04-06 17:38:41 +0530

    6th april 2026 5:38 pm

--- CygnetCI.Agent/obj/Release/net9.0/linux-x64/CygnetCI.Agent.AssemblyInfo.cs
+++ CygnetCI.Agent/obj/Release/net9.0/linux-x64/CygnetCI.Agent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CygnetCI.Agent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d95a4de8e1871035b995dde29cf6df14d13a0b15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9801e66006a2b737acf28eaf54f49317e077b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CygnetCI.Agent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CygnetCI.Agent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
